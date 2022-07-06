464dff8936c04f760ba357d615c6b4d8841ed63c
Daniel Lucena <daniel.lucena@olxbr.com>
2022-07-06 18:02:55 -0300

    add Formulario component state

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
 
-class Formulario extends Component {    
+class Formulario extends Component {   
+    state = {
+        mesas: ["Mesa 1", "Mesa 2", "Mesa 3", "Mesa 4"],
+        itens: ["Água", "Suco", "Sanduíche"],
+    }
+
     render(){
+        const cadastrarPedido = event => {
+            event.preventDefault();
+
+            console.log("Cadastrar");
+        }
         return(
             <>
                 <h1>Cadastre o pedido</h1>
-                <form>
+                <form onSubmit={cadastrarPedido}>
                     <label>Selecione a mesa</label>
                     <select id="mesa">
-                        <option value="Mesa 1">Mesa 1</option>
-                        <option value="Mesa 2">Mesa 2</option>
-                        <option value="Mesa 3">Mesa 3</option>
+                        { 
+                            this.state.mesas.map(mesa => 
+                                <option 
+                                    value={mesa}
+                                    onChange={event => this.setState({ mesa: event.target.value })}
+                                >
+                                    {mesa}
+                                </option>) 
+                        };
                     </select>
 
                     <label>Selecione os itens do pedido</label>
 
-                    <label for="agua">Água</label>
-                    <input type="checkbox" id="agua" name="item_pedido" value="Água" />
-
-                    <label for="agua">Suco</label>
-                    <input type="checkbox" id="suco" name="item_pedido" value="Suco" />
-  
-                    <label for="agua">Sanduíche</label>
-                    <input type="checkbox" id="sanduiche" name="item_pedido" value="Sanduíche" />
+                    { 
+                        this.state.itens.map(item => 
+                            <label>
+                                <input 
+                                    type="checkbox" 
+                                    name={item} 
+                                    value={item} 
+                                />
+                                {item}
+                            </label>) 
+                    };
 
                     <button type="submit">Fazer Pedido</button>
                 </form>
